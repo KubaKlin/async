7b44c51de7c88b7948eba9fff33895c0ad5d05d4
Kuba Klin <poczta@kubaklin.pl>
2025-01-31 13:43:34 +0100

    feat: tasks functions from 1 to 7

--- src/index.js
+++ src/index.js
@@ -1,66 +1,211 @@
 // 1
-function waitForAnyButtonToBeClicked(firstSelector, secondSelector) {
-  return new Promise(function (resolve, reject) {
-    const firstButton = document.querySelector(firstSelector);
-    const secondButton = document.querySelector(secondSelector);
-    if (!secondButton || !firstButton) {
-      reject();
+function divideIfNotZero(firstNumber, secondNumber) {
+  if (secondNumber === 0) {
+    throw new Error('second number should not be zero');
+  }
+  return firstNumber / secondNumber;
+}
+
+try {
+  console.log(divideIfNotZero(2, 0));
+} catch (error) {
+  console.log(error.message);
+}
+
+// 2
+function parseDateIfValid(dateString) {
+  const date = new Date(dateString);
+
+  if (isNaN(date.getTime())) {
+    throw new Error('Invalid date');
+  }
+
+  return date;
+}
+
+try {
+  console.log(parseDateIfValid('today'));
+} catch (error) {
+  console.error(error.message);
+}
+
+// 3
+function evaluateScript(scriptString) {
+  try {
+    return eval(scriptString);
+  } catch {
+    return null;
+  }
+}
+
+console.log(evaluateScript('1 + 5'));
+
+// 4
+function getUserName(usersID) {
+  return fetch(`https://jsonplaceholder.typicode.com/users/${usersID}`)
+    .then((response) => {
+      if (!response.ok) {
+        return null;
+      }
+      return response.json();
+    })
+    .then((user) => {
+      if (user) {
+        return user.name;
+      }
+      return null;
+    })
+    .catch(() => null);
+}
+getUserName(2).then((name) => console.log(name));
+
+async function getUserNameAsync(usersID) {
+  try {
+    const response = await fetch(
+      `https://jsonplaceholder.typicode.com/users/${usersID}`,
+    );
+    if (!response.ok) {
+      return null;
     }
-    if (firstButton) {
-      firstButton.addEventListener('click', function () {
-        resolve(firstButton);
-      });
-    }
-    if (secondButton) {
-      secondButton.addEventListener('click', function () {
-        resolve(secondButton);
-      });
-    }
+    const user = await response.json();
+    return user.name || null;
+  } catch {
+    return null;
+  }
+}
+getUserNameAsync(1).then((name) => console.log(name));
+
+// 5
+function getAllPostsFromUser(userId) {
+  return new Promise((resolve, reject) => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then((response) => response.json())
+      .then((posts) => {
+        const userPosts = posts.filter((post) => post.userId === userId);
+        if (userPosts.length > 0) {
+          resolve(userPosts);
+        } else {
+          reject(new Error(`No posts found for user with ID ${userId}`));
+        }
+      })
+      .catch((error) => reject(error));
   });
 }
 
-waitForAnyButtonToBeClicked('#first-button', '#second-button')
-  .then(function (clickedButton) {
-    console.log('A button was clicked', clickedButton);
-  })
-  .catch(function () {
-    console.log('One of the buttons does not exist');
-  });
+getAllPostsFromUser(5)
+  .then((posts) => console.log(posts))
+  .catch((error) => console.error(error));
 
-// 2
-function waitForPasswordsToMatch(firstSelector, secondSelector) {
-  return new Promise(function (resolve, reject) {
-    const passwordInputs = document.querySelectorAll('.password-input');
+async function getAllPostsFromUserAsync(userId) {
+  try {
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+    const posts = await response.json();
+    const userPosts = posts.filter((post) => post.userId === userId);
+    if (userPosts.length > 0) {
+      return userPosts;
+    } else {
+      throw new Error(`No posts found for user with ID ${userId}`);
+    }
+  } catch (error) {
+    throw error;
+  }
+}
 
-    passwordInputs.forEach(function (input) {
-      input.addEventListener('input', function () {
-        const firstInput = document.querySelector(firstSelector);
-        const secondInput = document.querySelector(secondSelector);
+getAllPostsFromUserAsync(1)
+  .then((posts) => console.log(posts))
+  .catch((error) => console.error(error));
 
-        if (!firstInput || !secondInput) {
-          reject();
-        } else {
-          const firstInputValue = firstInput.value;
-          const secondInputValue = secondInput.value;
-          const inputsMatch = firstInputValue === secondInputValue;
-
-          if (
-            inputsMatch &&
-            secondInputValue !== '' &&
-            firstInputValue !== ''
-          ) {
-            resolve();
-          }
-        }
-      });
+// 6
+function getAlbumsWithPhotos() {
+  return Promise.all([
+    fetch('https://jsonplaceholder.typicode.com/albums').then((response) =>
+      response.json(),
+    ),
+    fetch('https://jsonplaceholder.typicode.com/photos').then((response) =>
+      response.json(),
+    ),
+  ]).then(([albums, photos]) => {
+    return albums.map((album) => {
+      album.photos = photos.filter((photo) => photo.albumId === album.id);
+      return album;
     });
   });
 }
 
-waitForPasswordsToMatch('#password-inputss', '#confirm-password-input')
-  .then(function () {
-    console.log('Passwords match');
-  })
-  .catch(function () {
-    console.log('Passwords doesn`t match or are missing');
+getAlbumsWithPhotos().then((albumsWithPhotos) => {
+  console.log(albumsWithPhotos);
+});
+
+async function getAlbumsWithPhotosAsync() {
+  const [albumsResponse, photosResponse] = await Promise.all([
+    fetch('https://jsonplaceholder.typicode.com/albums'),
+    fetch('https://jsonplaceholder.typicode.com/photos'),
+  ]);
+
+  const albums = await albumsResponse.json();
+  const photos = await photosResponse.json();
+
+  return albums.map((album) => {
+    album.photos = photos.filter((photo) => photo.albumId === album.id);
+    return album;
   });
+}
+
+getAlbumsWithPhotosAsync().then((albumsWithPhotos) => {
+  console.log(albumsWithPhotos);
+});
+
+// 7
+function getUsersWithPostsAndComments() {
+  return Promise.all([
+    fetch('https://jsonplaceholder.typicode.com/users').then((response) =>
+      response.json(),
+    ),
+    fetch('https://jsonplaceholder.typicode.com/posts').then((response) =>
+      response.json(),
+    ),
+    fetch('https://jsonplaceholder.typicode.com/comments').then((response) =>
+      response.json(),
+    ),
+  ]).then(([users, posts, comments]) => {
+    return users.map((user) => {
+      user.posts = posts
+        .filter((post) => post.userId === user.id)
+        .map((post) => {
+          post.comments = comments.filter(
+            (comment) => comment.postId === post.id,
+          );
+          return post;
+        });
+      return user;
+    });
+  });
+}
+
+getUsersWithPostsAndComments().then((data) => console.log(data));
+
+async function getUsersWithPostsAndCommentsAsync() {
+  const [usersResponse, postsResponse, commentsResponse] = await Promise.all([
+    fetch('https://jsonplaceholder.typicode.com/users'),
+    fetch('https://jsonplaceholder.typicode.com/posts'),
+    fetch('https://jsonplaceholder.typicode.com/comments'),
+  ]);
+
+  const users = await usersResponse.json();
+  const posts = await postsResponse.json();
+  const comments = await commentsResponse.json();
+
+  return users.map((user) => {
+    user.posts = posts
+      .filter((post) => post.userId === user.id)
+      .map((post) => {
+        post.comments = comments.filter(
+          (comment) => comment.postId === post.id,
+        );
+        return post;
+      });
+    return user;
+  });
+}
+
+getUsersWithPostsAndCommentsAsync().then((data) => console.log(data));
